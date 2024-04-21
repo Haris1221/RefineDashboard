525a5495344adb987c4e3ff3c4e284da0d02b174
haris <Haris199x@gmail.com>
2024-04-20 23:56:36 -0500

    init

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,13 @@
+import { authCredentials } from "../../providers";
+import { AuthPage } from "@refinedev/antd";
+
+export const Login = () => {
+  return (
+    <AuthPage
+      type="login"
+      formProps={{
+        initialValues: authCredentials,
+      }}
+    />
+  );
+};
